42f4d642218edad0b60e0c4838efaf0583651edc
JuanManuelTrejoEspinoza <juan.espinoza@asa.financial>
2023-10-12 22:11:57 -0600

    First upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Deployment';
+
+	title = 'Deployment';
+	_myValue: boolean = false;
+
+	change() {
+		this._myValue = !this._myValue;
+	}
 }
